8f7f82c74f265859c4a42e82a03afdd57022f872
Admin <Admin@DESKTOP-GB1CE40>
2026-08-13 00:23:37 -0700

    Rename the project and solution

---
+++ LPGDataAnalyzer/Models/AppSettings.cs
@@ -0,0 +1,35 @@
+﻿namespace LPGDataAnalyzer.Models
+{
+    public class AppSettings
+    {
+        public string LastSavedFilePath { get; set; } = string.Empty;
+        public string LastLoadedFuelTable { get; set; } = string.Empty;
+
+        public string ImagePath { get; set; } =
+            Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.Desktop),
+                "Untitled.png");
+
+        /// <summary>
+        /// This is last suggested new fuel table based on last analysed data.
+        /// </summary>
+        public string LastPredictedFuelTable { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Point to the folder where your JSON snapshots are stored.
+        /// </summary>
+        public string HistoryFolder { get; set; } =
+            Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.UserProfile),
+                "Downloads",
+                "LPGDataAnalyzer",
+                "LPGDataAnalyzer",
+                "History");
+
+        public string DataFilesFolder { get; set; } =
+            Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),
+                "MultipointInj",
+                "Acquisition");
+    }
+}
